f8405c017020a8b77887d45ab7690656880350fd
kaikune <alexkai03@live.com>
2023-07-07 20:05:38 -0400

    Established Connection to Vertex API

--- NLPAtlas/atlasapp/views.py
+++ NLPAtlas/atlasapp/views.py
@@ -3,9 +3,24 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from django.http import HttpResponse
 from django.shortcuts import render
+import vertexai
+from vertexai.preview.language_models import CodeGenerationModel
 
 # Create your views here.
 
+def generateQuery(str):
+    vertexai.init(project="atlas-madness-392102", location="us-central1")
+    parameters = {
+        "temperature": 0.1,
+        "max_output_tokens": 1024
+    }
+    model = CodeGenerationModel.from_pretrained("code-bison@001")
+    response = model.predict(
+        prefix = str,
+        **parameters
+    )
+    print(f"Response from Model: {response.text}")
+    return response.text
 
 uri = "mongodb+srv://alexkai03:fDiGRgzlwU0MFS0V@cluster0.bdepqww.mongodb.net/?retryWrites=true&w=majority"
 
@@ -17,7 +32,7 @@ collection = db.listingsAndReviews
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    print("Successfully connected to MongoDB Atlas!")
 except Exception as e:
     print(e)
 
