306cf13fa287747c1ca1195846817f10fa95403b
kaikune <alexkai03@live.com>
2023-07-11 11:19:08 -0400

    Test different db

--- NLPAtlas/atlasapp/views.py
+++ NLPAtlas/atlasapp/views.py
@@ -16,21 +16,21 @@ uri = 'mongodb+srv://alexkai03:fDiGRgzlwU0MFS0V@cluster0.bdepqww.mongodb.net/?re
 prompt = 'You are a natural language to MongoDB query generator that only outputs code if 100percent certain of correctness.\
       Assume prerequisite code is supplied. All queries will be executed in python so it must follow python syntax. \
       In the format: \
-    db=client["database_name"] \
-    collection = db.collection_name \
+    db=client["{}"] \
+    collection = db.{} \
     #Do the mongoDB query here \
     cursor = #query result \
     The constraints are: \
-    The database name is "sample_airbnb". \
-    The collection is "listingsAndReviews". \
+    The database name is "sample_supplies". \
+    The collection is "sales". \
     There is a limit of 10 documents unless otherwise specified. \
     Instead of printing the result, store the result in cursor. \
     Write the query: "{}"'
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
-db = client['sample_airbnb']
-collection = db.listingsAndReviews
+# db = client['sample_supplies']
+# collection = db.sales
 
 
 # Send a ping to confirm a successful connection
@@ -55,7 +55,7 @@ def generateQuery(str):
     print('Generating query...')
     vertexai.init(project='atlas-madness-392102', location='us-central1')
     parameters = {
-        'temperature': 0.2,
+        'temperature': 0.1,
         'max_output_tokens': 1024
     }
     model = CodeGenerationModel.from_pretrained('code-bison@001')
@@ -70,41 +70,60 @@ def generateQuery(str):
 cursor = 'Cursor Empty'
 
 # Attempt to run generated query
-def runQuery(query):
-    print('Running query...')
-    try:
-        exec(query, globals())
-        return cursor
-    except Exception as e:
-        print(f'Invalid query: {e}')
-        return 'Bad query'
+def runQuery(query, db, collection):
+    def runQueryHelper():
+        print('Running query...')
+        try:
+            exec(query, globals())
+            return cursor
+        except Exception as e:
+            print(f'Invalid query: {e}')
+            return 'Bad query'
+    runQueryHelper()
 
 @csrf_exempt
 def main(request):
     userQuery = ''
     data = ''
     df = None
+    db = None
+    databaseNames = client.list_database_names()
     if request.method == 'POST':
         # Retrieve the userQuery value from the form data
         userQuery = request.POST.get('userQuery', '') 
+        db = request.POST.get('database')
+        collection = request.POST.get('collection')
 
+        collectionNames = []
+        if db:
+            # Get the list of collections for the selected database
+            collectionNames = client[db].list_collection_names()
+
+        userQuery = prompt.format(db, collection, userQuery)
         print('User Query:', userQuery)
-        userQuery = prompt.format(userQuery)
         result = generateQuery(userQuery)
-#         result = 'df = client["sample_airbnb"]\n\
-# collection = db.listingsAndReviews\n\
-# cursor = collection.find({}, {"name": 1, "_id": 0}).limit(5)'
+
         if isValidPython(result):
-            data = runQuery(result)
+            data = runQuery(result, db, collection)
             df = pd.DataFrame(data)
             print(df)
             df = df.to_html(classes='table table-dark table-hover')
         else:
             print('Bad python generated')
+
+        context = {
+            'userQuery': userQuery,
+              'result': result, 
+              'dataframe': df, 
+              'databaseNames': databaseNames, 
+              'collectionNames': collectionNames,
+              'selectedDB': db,
+              'selectedCollection': collection
+            }
     else:
         userQuery = ''  # Set the initial value of userQuery when the page is first loaded
-        result = 'Error has occurred' 
-
-    context = {'userQuery': userQuery, 'result': result, 'dataframe': df}
+        result = '' 
+        context = {'databaseNames': databaseNames, 'userQuery': userQuery, 'result': result}
+    
     return render(request, 'master.html', context)
 
